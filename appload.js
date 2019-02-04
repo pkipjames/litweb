c821b62f7496ecf06d9b6ecaffe0e15ccb5b618b
James M <45425618+pkipjames@users.noreply.github.com>
2019-02-04 07:49:55 -0500

    Create appload.js

---
+++ appload.js
@@ -0,0 +1,33 @@
+var appArray=[
+{name:"Malone Browser",url:"malone.html"}
+
+];
+function elt(name, attributes) {
+  var node = document.createElement(name);
+  if (attributes) {
+    for (var attr in attributes)
+      if (attributes.hasOwnProperty(attr))
+        node.setAttribute(attr, attributes[attr]);
+  }
+  for (var i = 2; i < arguments.length; i++) {
+    var child = arguments[i];
+    if (typeof child == "string")
+      child = document.createTextNode(child);
+    node.appendChild(child);
+  }
+  return node;
+}
+function runAppMenu(){
+var container=document.querySelector("#appsMenu");
+if(localStorage.getItem("apps")){
+appArray=JSON.parse(localStorage.getItem("apps"));
+}else{
+localStorage.setItem("apps",JSON.stringify(appArray));
+}
+
+for (var i=0;i<appArray.length;i++){
+var item=appArray[i];
+container.appendChild(elt("li",null,elt("a",{href:item.url,target:"appwindow"},item.name)));
+}
+
+}
