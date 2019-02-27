57f15a8bc711a783945ae2a8ed61ae51a1c3d1b9
James M <45425618+pkipjames@users.noreply.github.com>
2019-02-27 07:42:45 -0500

    Update appstore.js

--- appstore.js
+++ appstore.js
@@ -4,7 +4,8 @@ description:"iOS Emulator Application"},
 {name:"Android Emulator",icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQahGOtHJtxGGD1nXogE03vOaxArAP7PpuSog6IhHwLdfBEytZuBQ:https://cnet4.cbsistatic.com/img/QJcTT2ab-sYWwOGrxJc0MXSt3UI%3D/2011/10/27/a66dfbb7-fdc7-11e2-8c7c-d4ae52e62bcc/android-wallpaper5_2560x1600_1.jpg",
 url:"https://appetize.io/embed/standalone_05tnrmc2uh5vzuhq911b2z1qrg?device=nexus9&scale=75&orientation=landscape&osVersion=9.0&deviceColor=black",
 description:"Android Emulator Application"},
-
+  {name:"Photobarcelona",url:"http://elizabethcastro.com/html6ed/examples/styles-handheld/handheld-adjust.html",
+   description:"Photos Of Barcelona"}
 
 
 ];
@@ -17,6 +18,7 @@ appArray.forEach(function (item){
   var icon=item.icon||"placeholderapp.png";
 container.appendChild(elt("div",null,
 elt("img",{height:100,alt:"icon",src:icon}),
+                          elt("p",null,item.name),
 elt("p",null,item.description,elt("button",{onclick:function (){installApp(item.name,item.url,item.icon}},"Install Application")));
 });
 }catch(err){alert(err);}
