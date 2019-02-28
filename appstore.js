afef741509c54f834c8b512e338b852aa4bbc8f7
James M <45425618+pkipjames@users.noreply.github.com>
2019-02-28 14:26:32 -0500

    Update appstore.js

--- appstore.js
+++ appstore.js
@@ -1,4 +1,4 @@
-var appArray=[
+var newAppArray=[
 {name:"iOS Emulator",icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuyb2tRjS8m3zgWIoxjsEjOB6jXXkrBBDtVlrN0BgvDAOgThcL:https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/IOS_logo.svg/300px-IOS_logo.svg.png",url:"https://appetize.io/embed/standalone_05tnrmc2uh5vzuhq911b2z1qrg?device=ipadair2&scale=75&orientation=landscape&osVersion=12.1&deviceColor=black",
 description:"iOS Emulator Application"},
 {name:"Android Emulator",icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQahGOtHJtxGGD1nXogE03vOaxArAP7PpuSog6IhHwLdfBEytZuBQ:https://cnet4.cbsistatic.com/img/QJcTT2ab-sYWwOGrxJc0MXSt3UI%3D/2011/10/27/a66dfbb7-fdc7-11e2-8c7c-d4ae52e62bcc/android-wallpaper5_2560x1600_1.jpg",
@@ -11,12 +11,27 @@ description:"Android Emulator Application"},
 
 
 ];
+function elt(name, attributes) {
+  var node = document.createElement(name);
+  if (attributes) {
+    for (var attr in attributes)
+      if (attributes.hasOwnProperty(attr))
+        node.setAttribute(attr, attributes[attr]);
+  }
+  for (var i = 2; i < arguments.length; i++) {
+    var child = arguments[i];
+    if (typeof child == "string")
+      child = document.createTextNode(child);
+    node.appendChild(child);
+  }
+  return node;
+}
 
 function runAppStore(){try{
 var container=document.querySelector("#appStoreIcons");
 
 
-appArray.forEach(function (item){
+newAppArray.forEach(function (item){
   var icon=item.icon||"placeholderapp.png";
 container.appendChild(elt("div",null,
 elt("img",{height:100,alt:"icon",src:icon}),
@@ -26,3 +41,4 @@ elt("p",null,item.description,elt("button",{onclick:function (){installApp(item.
                       });}catch(err){alert(err);}
 
 }
+
