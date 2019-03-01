f9fbf223a8ec7abc59a077c0ef034096c79276fd
James M <45425618+pkipjames@users.noreply.github.com>
2019-03-01 12:22:35 -0500

    Update appload.js

--- appload.js
+++ appload.js
@@ -116,7 +116,7 @@ appArray=JSON.parse(localStorage.getItem("apps"));
 localStorage.setItem("apps",JSON.stringify(appArray));console.log(localStorage.getItem("apps"));
 }
 
-appArray.forEach(function (item){var icon=item.icon||"placeholderapp.png";if(item.js){container.appendChild(elt("li",{style:"list-style-image:url(\""+icon+"\");list-style-position:inside;"},elt("a",{href:item.url,onclick:item.js,target:"appwindow"},item.name)));}else{container.appendChild(elt("li",{style:"list-style-image:url(\""+icon+"\");list-style-position:inside;"},elt("a",{href:item.url,target:"appwindow"},item.name)));}});
+appArray.forEach(function (item){var icon=item.icon||"placeholderapp.png";if(item.js){container.appendChild(elt("li",{style:""},elt("a",{href:item.url,onclick:item.js,target:"appwindow"},item.name)));}else{container.appendChild(elt("li",{style:""},elt("a",{href:item.url,target:"appwindow"},item.name)));}});
 }catch(err){alert(err);}
 
 }
