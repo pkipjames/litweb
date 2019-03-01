99ccf3c6bd71a6a9e97e18e9690e38c4d754f4d4
James M <45425618+pkipjames@users.noreply.github.com>
2019-03-01 12:28:06 -0500

    Update appstore.js

--- appstore.js
+++ appstore.js
@@ -1,3 +1,20 @@
+var appArray;
+function installApp(name,url,icon,description){
+  appArray=JSON.parse(localStorage.getItem("apps"));
+  console.log(JSON.stringify(appArray));
+  localStorage.setItem("apps",JSON.stringify(appArray));
+	var app={name:name,url:url};
+	if(icon){app["icon"]=icon;}
+	if(description){app["description"]=description;}
+  appArray.push(app);
+    console.log(JSON.stringify(appArray));
+
+  localStorage.setItem("apps",JSON.stringify(appArray));
+  
+  console.log(JSON.stringify(appArray));
+
+}
+
 var newAppArray=[
 {name:"iOS Emulator",icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuyb2tRjS8m3zgWIoxjsEjOB6jXXkrBBDtVlrN0BgvDAOgThcL:https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/IOS_logo.svg/300px-IOS_logo.svg.png",url:"https://appetize.io/embed/standalone_05tnrmc2uh5vzuhq911b2z1qrg?device=ipadair2&scale=75&orientation=landscape&osVersion=12.1&deviceColor=black",
 description:"iOS Emulator Application"},
@@ -33,10 +50,12 @@ var container=document.querySelector("#appStoreIcons");
 
 newAppArray.forEach(function (item){
   var icon=item.icon||"placeholderapp.png";
+  var mainInstallBTN=elt("button",{onclick:function (){installApp(window.prompt("Use App Name: ",item.name),item.url,item.icon,item.description);}},"Install");
 container.appendChild(elt("div",null,
 elt("img",{height:100,alt:"icon",src:icon}),
                           elt("p",null,item.name),
-elt("p",null,item.description,elt("button",{onclick:function (){installApp(window.prompt("Use App Name: ",item.name),item.url,item.icon,item.description);}},"Install Application"))));
+                         
+elt("p",null,item.description,mainInstallBTN)));
 
                       });}catch(err){alert(err);}
 
