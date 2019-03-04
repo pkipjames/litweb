72a985f96836402e14cacd85ac3b38640694ce2f
James M <45425618+pkipjames@users.noreply.github.com>
2019-03-04 13:51:44 -0500

    Update update.js

--- update.js
+++ update.js
@@ -3,7 +3,9 @@ function checkForUpdates(){
 if(localStorage.getItem("osVersion")){
 var memory=localStorage.getItem("osVersion");
 if(memory!=version){
-if(window.confirm("Update avaible. Update now")){localStorage.clear();location.reload();}
+if(window.confirm("Update avaible. Update now? (It only takes pretty much 1 page reload)")){localStorage.clear();location.reload();}
+}else{
+ console.log("System is running latest software :-)"); 
 }
 }else{
 localStorage.clear();
