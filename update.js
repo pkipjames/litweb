4c687d588749b16b7489e421eddd8a4e77c8b821
James M <45425618+pkipjames@users.noreply.github.com>
2019-03-12 14:14:53 -0400

    Update update.js

--- update.js
+++ update.js
@@ -1,9 +1,16 @@
-var version="2.1";
+var version="3.0";
 function checkForUpdates(){
 if(localStorage.getItem("osVersion")){
 var memory=localStorage.getItem("osVersion");
 if(memory!=version){
-if(window.confirm("Update avaible. Update now? (It only takes pretty much 1 page reload)")){localStorage.clear();location.reload();}
+if(window.confirm("Update avaible. Update now? (It only takes pretty much 1 page reload)")){localStorage.clear();
+                                                                                            try{var appCache = window.applicationCache;
+
+appCache.update(); /*this will attempt to update the users cache and changes the application cache status to 'UPDATEREADY'.*/
+
+if (appCache.status == window.applicationCache.UPDATEREADY) {
+  appCache.swapCache(); /*replaces the old cache with the new one.*/
+}}catch(err3){}location.reload();}
 }else{
  console.log("System is running latest software :-)"); 
 }
