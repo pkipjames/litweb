607cacc799b16c5ec426dcf30f3986a07494d849
James M <45425618+pkipjames@users.noreply.github.com>
2019-03-12 07:47:30 -0400

    Update bluetooth.js

--- bluetooth.js
+++ bluetooth.js
@@ -3,3 +3,25 @@ unction bluetoothConnect() {
 	  .then(function(){alert("Connected to: "+device.name});})
 	  .catch(console.error);
 }
+
+function isBT(){
+if(navigator.bluetooth){	
+	setInterval(function(){
+		if(navigator.bluetooth.checkAvaibility()){
+			document.querySelector("#btIcon1").style.display="inline";
+
+		}else{
+			document.querySelector("#btIcon1").style.display="none";
+
+			
+		}
+		
+	},10000);
+	
+	
+}else{
+document.querySelector("#btIcon1").style.display="none";
+	
+	
+}
+}
