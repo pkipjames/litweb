f6e906dd2701550463400b2726083422bde8f63c
James M <45425618+pkipjames@users.noreply.github.com>
2019-03-13 07:45:13 -0400

    Update makelinkswork.js

--- makelinkswork.js
+++ makelinkswork.js
@@ -13,7 +13,7 @@ if(("standalone" in window.navigator) && window.navigator.standalone){
 	        noddy = noddy.parentNode;
 	    }
 		
-		if(noddy.target='appwindow'&&'href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes))
+		if(noddy.target=='appwindow'&&'href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes))
 		{
 			event.preventDefault();
 			document.getElementById("appwindow1").setAttribute("src",noddy.href);
