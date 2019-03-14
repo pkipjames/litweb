d6ab69bfb25d7a119c96c4898b35107d04ebf0a3
James M <45425618+pkipjames@users.noreply.github.com>
2019-03-14 13:44:30 -0400

    Update update.js

--- update.js
+++ update.js
@@ -1,46 +1,55 @@
-var version="5.2";
-function checkForUpdates(){
-if(localStorage.getItem("osVersion")){
-var memory=localStorage.getItem("osVersion");
- 
-if(memory!=version){
-if(window.confirm("Update avaible. Update now? (It only takes pretty much 1 page reload)")){
-  for (var i=0;i<localStorage.length;i++){
-  var key=localStorage.key(i);
-   if(key!="apps"){
-    localStorage.removeItem(key);
-   }else{
-     /*While the rest are deleted normally, the apps are more delicate. No one wants to have to reinstall all their apps.*/
-     if(appArray){
-      for (var j=0;j<appArray.length;j++){
-        var appsFromStorage=JSON.parse(localStorage.getItem("apps"));
-        var item=appArray[j];
-           if(!(item in appsFromStorage)){
-             appsFromStorage.push(item);
-           }
-        
-        
-        
-      }
-     }
-   }
-   
-  }
-                                                                                            try{var appCache = window.applicationCache;
+var litwebversion = "5.2";
 
-appCache.update(); /*this will attempt to update the users cache and changes the application cache status to 'UPDATEREADY'.*/
+function checkForUpdates() {
+    if (localStorage.getItem("osVersion")) {
+        var memory = localStorage.getItem("osVersion");
 
-if (appCache.status == window.applicationCache.UPDATEREADY) {
-  appCache.swapCache(); /*replaces the old cache with the new one.*/
-}}catch(err3){}location.reload();}
-}else{
- console.log("System is running latest software :-)"); 
-}
-}else{
-localStorage.clear();
-localStorage.setItem("osVersion",version);
-location.reload();
-}
+        if (memory != litwebversion) {
+            if (window.confirm("Update avaible. Update now? (It only takes pretty much 1 page reload)")) {
+                for (var i = 0; i < localStorage.length; i++) {
+                    var key = localStorage.key(i);
+                    if (key != "apps") {
+                        localStorage.removeItem(key);
+                    } else {
+                        /*While the rest are deleted normally, the apps are more delicate. No one wants to have to reinstall all their apps.*/
+                        if (appArray) {
+                            for (var j = 0; j < appArray.length; j++) {
+                                var appsFromStorage = JSON.parse(localStorage.getItem("apps"));
+                                var item = appArray[j];
+                                if (!(item in appsFromStorage)) {
+                                    appsFromStorage.push(item);
+                                }
+
+
+
+                            }
+                        }
+                    }
+
+                }
+                try {
+                    var appCache = window.applicationCache;
+
+                    appCache.update(); /*this will attempt to update the users cache and changes the application cache status to 'UPDATEREADY'.*/
+
+                    if (appCache.status == window.applicationCache.UPDATEREADY) {
+                        appCache.swapCache(); /*replaces the old cache with the new one.*/
+                    }
+                } catch (err3) {}
+                     localStorage.setItem("osVersion", litwebversion);
+
+                location.reload();
+            }
+        } else {
+            console.log("System is running latest software :-)");
+        }
+    } else {
+        localStorage.clear();
+        localStorage.setItem("osVersion", litwebversion);
+        location.reload();
+    }
 }
 
-setTimeout(function (){checkForUpdates();},10000);
+setTimeout(function() {
+    checkForUpdates();
+}, 10000);
