4263001bf20b57b568304d812868f348626206d0
James M <45425618+pkipjames@users.noreply.github.com>
2019-03-14 13:32:31 -0400

    Update update.js

--- update.js
+++ update.js
@@ -2,8 +2,30 @@ var version="4.0";
 function checkForUpdates(){
 if(localStorage.getItem("osVersion")){
 var memory=localStorage.getItem("osVersion");
+ 
 if(memory!=version){
-if(window.confirm("Update avaible. Update now? (It only takes pretty much 1 page reload)")){localStorage.clear();
+if(window.confirm("Update avaible. Update now? (It only takes pretty much 1 page reload)")){
+  for (var i=0:i<localStorage.length;i++){
+  var key=localStorage.key(i);
+   if(key!="apps"){
+    localStorage.removeItem(key);
+   }else{
+     /*While the rest are deleted normally, the apps are more delicate. No one wants to have to reinstall all their apps.*/
+     if(appArray){
+      for (var i=0;i<appArray.length;i++){
+        var appsFromStorage=JSON.parse(localStorage.getItem("apps"));
+        var item=appArray[i];
+           if(!(item in appsFromStorage)){
+             appsFromStorage.push(item);
+           }
+        
+        
+        
+      }
+     }
+   }
+   
+  }
                                                                                             try{var appCache = window.applicationCache;
 
 appCache.update(); /*this will attempt to update the users cache and changes the application cache status to 'UPDATEREADY'.*/
