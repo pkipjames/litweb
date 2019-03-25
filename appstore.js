9a7ff18664ac0add7a6b7c8b416431b3734bc86c
James M <45425618+pkipjames@users.noreply.github.com>
2019-03-25 14:05:55 -0400

    Update appstore.js

--- appstore.js
+++ appstore.js
@@ -18,6 +18,8 @@ function installApp(name,url,icon,description){
 var newAppArray=[
 {name:"iOS Emulator",icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuyb2tRjS8m3zgWIoxjsEjOB6jXXkrBBDtVlrN0BgvDAOgThcL:https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/IOS_logo.svg/300px-IOS_logo.svg.png",url:"https://appetize.io/embed/standalone_05tnrmc2uh5vzuhq911b2z1qrg?device=ipadair2&scale=75&orientation=landscape&osVersion=12.1&deviceColor=black",
 description:"iOS Emulator Application"},
+	{name:"Virtual-Fireplace.net- the cosiest place online!",url:"http://www.virtual-fireplace.net",
+	 description:"Virtual Fireplaces"},
 {name:"Android Emulator",icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQahGOtHJtxGGD1nXogE03vOaxArAP7PpuSog6IhHwLdfBEytZuBQ:https://cnet4.cbsistatic.com/img/QJcTT2ab-sYWwOGrxJc0MXSt3UI%3D/2011/10/27/a66dfbb7-fdc7-11e2-8c7c-d4ae52e62bcc/android-wallpaper5_2560x1600_1.jpg",
 url:"https://appetize.io/embed/standalone_05tnrmc2uh5vzuhq911b2z1qrg?device=nexus9&scale=75&orientation=landscape&osVersion=9.0&deviceColor=black",
 description:"Android Emulator Application"},
