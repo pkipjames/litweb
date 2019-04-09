6307c869d093fd531130b0d6766c59971e586e1e
James M <45425618+pkipjames@users.noreply.github.com>
2019-04-09 12:40:59 -0400

    Update desktop.js

--- desktop.js
+++ desktop.js
@@ -1,1 +1,28 @@
 /*desktop javascript code*/
+var desktopItems=[
+
+];
+function fixDesktopUp(){
+ if(localStorage.getItem("desktopItems")){
+   desktopItems=JSON.parse(localStorage.getItem("desktopItems"));
+   
+ }else{
+  localStorage.setItem("desktopItems",JSON.stringify(desktopItems));
+  
+ }
+}
+
+function addToDesktop(item){
+ desktopItems.push(item);
+    localStorage.setItem("desktopItems",JSON.stringify(desktopItems));
+}
+
+function showDesktop(){
+ fixDesktopUp();
+  document.body.innerHTML="";
+  for (var j=0;j<desktopItems.length;j++){
+    var curItem=desktopItems[j];
+   document.body.appendChild(elt("iframe",{style:"border:0px none;display:inline;",height:curItem.height,width:curItem.width,src:curItem.src},curItem.name+" widget"));
+   
+  }
+}
